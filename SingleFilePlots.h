aee08cbee535396ea20c23dd7b827ead28e7b2fd
Cheryl Patrick <c.patrick@ucl.ac.uk>
2018-02-28 17:45:40 +0000

    Basic template for Pawel's requests

---
+++ SingleFilePlots.h
@@ -0,0 +1,19 @@
+#include "TFile.h"
+#include "TTree.h"
+#include "TH1.h"
+#include "TROOT.h"
+#include "TCanvas.h"
+#include "TMath.h"
+#include "TLegend.h"
+#include "TStyle.h"
+#include "TLimit.h"
+#include "TError.h"
+#include "TLimitDataSource.h"
+#include "TConfidenceLevel.h"
+#include <iostream>
+
+using namespace std;
+int main();
+
+void MakeSingleFilePlots(string filename);
+
